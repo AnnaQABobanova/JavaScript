941d4a921a6f6207868ffadb912a7cefc5d53188
AnnaQABobanova <152427473+AnnaQABobanova@users.noreply.github.com>
2023-12-28 19:34:48 +0200

    Update homework1.js

--- homework1.js
+++ homework1.js
@@ -1,8 +1,10 @@
 //homeWork1
-let value;
-//value = ;
-if(typeof value !== "number" || value !== value){
-    console.log("It's not a number")
-} else{
-   console.log("it's a number")
+function myIsNaN(value){
+    if(typeof value !== "number" || value !== value){
+        console.log("It's not a number")
+    } else{
+       console.log("it's a number")
+    }
 }
+//let userInput = ;
+//console.log(myIsNaN(userInput))
